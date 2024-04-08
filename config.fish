e07141cf7c773c6caea478968e4c4b40a9d34b89
shell <shell@deb>
2024-04-07 18:03:14 -0600

    gave -iv aliases

--- config.fish
+++ config.fish
@@ -7,8 +7,7 @@ end
 
 alias cl="clear"
 alias code="cd ~/Documents/code"
-alias l="ls"
-alias ll="ls -a"
+alias cp="cp -iv"
 #alias dir='dir --color=auto'
 #alias du="du -h -c"
 alias du="ncdu"
@@ -18,11 +17,15 @@ alias e="exit"
 alias games="cd /mnt/Third/SSD_Games"
 #alias grep='grep --color=auto'
 #alias la='ls -A'
+alias ll="ls -a"
 #alias ll='ls -l'
+alias l="ls"
 #alias l='ls -CF'
 alias log="journalctl -f"
 #alias ls='ls --color=auto'
+alias mv="mv -iv"
 alias python="python3"
+alias rm="rm -iv"
 alias startenv="source /mnt/Second/.other/env/bin/activate"
 alias update="yes | sudo nala update; yes | sudo nala upgrade"
 zoxide init fish | source
