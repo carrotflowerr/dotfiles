8fb726bb8090d3dd0e7dfd68db5114a5f2484bf4
shell <shell@deb>
2024-05-15 16:12:57 -0600

    idk

--- fish/config.fish
+++ fish/config.fish
@@ -14,23 +14,26 @@ pfetch
 set -Ua fish_glob_dotfiles
 
 # Aliases
+
+
 alias cl="clear"
-alias playNew="ls --sort oldest | xargs -d '\n' mpv"
-alias code="cd ~/Documents/code"
 alias cp="cp -iv"
 alias du="ncdu"
 alias e="exit"
+alias ec="emacsclient -c"
 alias games="cd /mnt/Third/SSD_Games"
 alias l="ls"
 alias ll="ls -a"
 alias log="journalctl -f"
 alias ls="exa"
 alias mv="mv -iv"
+alias playNew="ls --sort oldest | xargs -d '\n' mpv --loop-playlist"
 alias pstree="ps -ejH"
 alias python="python3"
 alias rm="rm -iv"
+alias sudo="doas"
 alias tarD="tar xvf"
-alias update="yes | doas apt update; yes | doas apt upgrade"
+alias update="yes | doas apt update; doas apt upgrade"
 
 # Virtual python environment
 set VIRTUAL_ENV "/mnt/Second/.other/env/bin/activate"
